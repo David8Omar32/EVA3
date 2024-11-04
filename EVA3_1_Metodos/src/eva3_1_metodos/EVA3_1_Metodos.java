feb3d58a59388242bfa3d71a2b4e1465fc6adb84
David8Omar32 <L24550248@chihuahua2.tecnm.mx>
2024-11-04 16:51:35 -0600

    Add files via upload

---
+++ EVA3_1_Metodos/src/eva3_1_metodos/EVA3_1_Metodos.java
@@ -0,0 +1,9 @@
+package eva3_1_metodos;
+public class EVA3_1_Metodos {
+    public static void main(String[] args) {
+        message("Un mensaje!!");
+    }
+    public static void message(String mensaje){
+        System.out.println("Tu mensaje: " + mensaje);
+    }
+}
